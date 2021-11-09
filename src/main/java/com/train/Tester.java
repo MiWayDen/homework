5b7d2189b67f32da4a9b454d88e2bbf07b70c214
s046109@gmail.com <wayden3414>
2021-11-09 18:12:07 +0800

    add while to continue or finish the booking system

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,13 +5,27 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter number of tickets: ");
-        int tickets = scanner.nextInt();
-        System.out.print("How many round-trip tickets:  ");
-        int round = scanner.nextInt();
-        Ticket ticket = new Ticket(tickets,round);
-        ticket.print();
-
+        int tickets = 0;
+        int key = 0;
+        int end = -1;
+        int loop = 1;
+        while (tickets != end) {
+//            System.out.println("loop "+loop);
+            key = loop;
+            System.out.print("loop "+loop+", Please enter number of tickets, or enter -1 to finish booking");
+            loop=loop+1;
+            tickets = scanner.nextInt();
+            if (tickets == end){
+                System.out.println("Thank you for this booking.");
+            } else {
+            System.out.print("How many round-trip tickets: ");
+            int round = scanner.nextInt();
+            Ticket ticket = new Ticket(tickets, round);
+            ticket.print();
+            }
+//            System.out.println("if want to continue please press enter any number, want to leave please enter -1");
+//            key = scanner.nextInt();
+        }
 
     }
 }
--- src/main/kotlin/com/train/Tester.kt
+++ src/main/kotlin/com/train/Tester.kt
@@ -4,13 +4,22 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
-    print("Please enter number of tickets: ")
-    val tickets = scanner.nextInt()
-    print("How many round-trip tickets:  ")
-    val round = scanner.nextInt()
-    var ticket = ticketcount(tickets,round)
-    ticket.print()
-
+    var tickets = 0
+    val end = -1
+    var loop = 1
+    while (tickets != end) {
+        print("Loop $loop Please enter number of tickets, or enter -1 to finish the booking:　")
+        tickets = scanner.nextInt()
+        if (tickets == end){
+            println("Thank you for this booking")
+        } else {
+            print("How many round-trip tickets: ")
+            var round = scanner.nextInt()
+            var ticket = ticketcount(tickets, round)
+            ticket.print()
+            loop =loop + 1
+        }
+        }
 }
 
 
@@ -19,10 +28,10 @@ class ticketcount(var alltickets:Int,var roundtrip:Int) {
     val round = 1800;
     val total = (alltickets-roundtrip)*oneway+roundtrip*round
     fun print(){
-        println("Total tickets: $alltickets")
-        println("Round-trip: $roundtrip")
-        println("Total: $total")
-        println("Total tickets: $alltickets"+"\n"+"Round-trip: $roundtrip"+"\n"+"Total: $total")
+        println("Total tickets: $alltickets\nRound-trip: $roundtrip\nTotal: $total")
+//        println("Round-trip: $roundtrip")
+//        println("Total: $total")
+//        println("Total tickets: $alltickets"+"\n"+"Round-trip: $roundtrip"+"\n"+"Total: $total")
 
     }
 }
